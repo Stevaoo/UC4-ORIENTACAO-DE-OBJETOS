f69a227e96f315b7d6e6bee0b0179f4ed82d772b
Stevão Rodrigues <162615972+Stevaoo@users.noreply.github.com>
2024-08-19 20:40:09 -0300

    Update 01.ts

--- AULA 6/EX01/01.ts
+++ AULA 6/EX01/01.ts
@@ -1,9 +1,9 @@
-let rl = require("readline-sync")
+export let rl = require("readline-sync")
 
 export interface Contador{
     valorContador: number;
     zerar(): void;
-    incrimentar(): void;
+    incrementar(): void;
     valor(): number;
 }
 
@@ -20,7 +20,7 @@ export class Contador implements Contador{
     }
 
     // Incrimento em um em um
-    public incrimentar(): void {
+    public incrementar(): void {
         this.valorContador += 1;
         // OU this.valorContador = this.valorContador + this.valorContador 
     }
@@ -30,11 +30,38 @@ export class Contador implements Contador{
     }
 }
 
-let contador1 = new Contador(10)
-console.log(contador1.valorContador);
 
-contador1.incrimentar()
-console.log(contador1.valorContador)
+function criarContador(): Contador {
+    let valorCriado = rl.questionInt("Defina o valor inicial do contador: ");
+    return new Contador(valorCriado);
+}
 
-contador1.zerar()
-console.log(contador1.valorContador)
+let contador1 = criarContador();
+
+let menu: boolean = true;
+while (menu) {
+    let opcao = rl.questionInt('Insira o numero da opcao desejada:\n1 - Definir Valor Inicial\n2 - Visualizar Contador\n3 - Incrementar Contador\n4 - Zerar Contador\n5 - Sair\n');
+
+    switch(opcao) {
+        case 1:
+            contador1 = criarContador();
+            break;
+        case 2:
+            console.log("Valor do Contador:", contador1.valor());
+            break;
+        case 3:
+            contador1.incrementar();
+            break;
+        case 4:
+            contador1.zerar();
+            break;
+        case 5:
+            menu = false;
+            break;
+        default:
+            console.log("Oppco invalida. Tente novamente.");
+            break;
+    }
+}
+
+console.log("Programa encerrado.");
