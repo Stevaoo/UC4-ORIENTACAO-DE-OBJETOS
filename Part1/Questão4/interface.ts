6e26f33b2e1b9877db99b06f3c2c896880f2af0d
Stevão Rodrigues <162615972+Stevaoo@users.noreply.github.com>
2024-08-20 13:04:41 -0300

    Delete Part1/Questão4/interface.ts

--- Part1/Questão4/interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Prova {
-    constructor(gabarito: Gabarito);
-    respostaAluno(resposta: string): void;
-    acertos(): number;
-    nota(): number;
-    maior(outraProva: Prova): number;
-}
